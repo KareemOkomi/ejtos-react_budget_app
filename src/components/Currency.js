d3a6d31b64fadde9cdccd766b6d59dea47ed8819
kareemokomi <okomikareem@gmail.com>
2024-01-27 13:26:31 -0500

    added all necessary changs

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,8 +2,11 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
-  const {dispatch } = useContext(AppContext);
+  
+    const {dispatch } = useContext(AppContext);
+    
     const changeCurrency = (val)=>{
+
             dispatch({
                 type: 'CHG_CURRENCY',
                 payload: val,
@@ -13,8 +16,7 @@ const Currency = () => {
   return (
         <div className='alert alert-secondary'> Currency {
       <select name="Currency" id="Currency" onChange={event=>changeCurrency(event.target.value)}>
-        <option defaultValue>(£ Pound)</option>
-        <option value="$">$ Dollar</option>
+        <option value='$'>$ Dollar</option>
         <option value="£">£ Pound</option>
         <option value="€">€ Euro</option>
         <option value="₹">₹ Ruppee</option>
